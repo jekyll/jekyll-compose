30e3a51aa6829b87b70038315f015d6edc09068e
Parker Moore <parkrmoore@gmail.com>
2014-05-10 15:36:23 -0400

    Initial import.

---
+++ lib/jekyll/compose.rb
@@ -0,0 +1,7 @@
+require "jekyll/compose/version"
+
+module Jekyll
+  module Compose
+    # Your code goes here...
+  end
+end
---
+++ lib/jekyll/compose/version.rb
@@ -0,0 +1,5 @@
+module Jekyll
+  module Compose
+    VERSION = "0.0.1"
+  end
+end
