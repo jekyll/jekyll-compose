710ddc40e3998ace65fc55b8431e9799cba8d5e1
Dean Silfen <dean.silfen@gmail.com>
2016-03-16 20:31:28 -0400

    Revert "Update tests to work for ruby 1.9"
    This reverts commit 553dbbbd1d56752ded289a8b1ad0e7496ff96afa.

--- spec/file_info_spec.rb
+++ spec/file_info_spec.rb
@@ -1,37 +1,44 @@
 RSpec.describe(Jekyll::Compose::FileInfo) do
-  let(:open_and_closing_tag) { "---\n" }
-  let(:layout_content) { "post\n" }
-
   describe '#content' do
     context 'with a title of only words' do
-      let(:expected_title) { "A test arg parser\n" }
-      subject { described_class.new Jekyll::Compose::ArgParser.new(
+      let(:expected_result) {<<-CONTENT.gsub(/^\s+/, '')
+          ---
+          layout: post
+          title: A test arg parser
+          ---
+        CONTENT
+      }
+
+      let(:parsed_args) { Jekyll::Compose::ArgParser.new(
           ['A test arg parser'],
           {}
         )
       }
 
       it 'does not wrap the title in quotes' do
-        expect(subject.content).to start_with(open_and_closing_tag)
-        expect(subject.content).to end_with(open_and_closing_tag)
-        expect(subject.content).to match(layout_content)
-        expect(subject.content).to match(expected_title)
+        file_info = described_class.new parsed_args
+        expect(file_info.content).to eq(expected_result)
       end
     end
 
     context 'with a title that includes a colon' do
-      let(:expected_title) { "'A test: arg parser'\n" }
-      subject { described_class.new Jekyll::Compose::ArgParser.new(
+      let(:expected_result) {<<-CONTENT.gsub(/^\s+/, '')
+          ---
+          layout: post
+          title: 'A test: arg parser'
+          ---
+        CONTENT
+      }
+
+      let(:parsed_args) { Jekyll::Compose::ArgParser.new(
           ['A test: arg parser'],
           {}
         )
       }
 
       it 'does wrap the title in quotes' do
-        expect(subject.content).to start_with(open_and_closing_tag)
-        expect(subject.content).to end_with(open_and_closing_tag)
-        expect(subject.content).to match(layout_content)
-        expect(subject.content).to match(expected_title)
+        file_info = described_class.new parsed_args
+        expect(file_info.content).to eq(expected_result)
       end
     end
   end
